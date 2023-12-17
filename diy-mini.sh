913a937de238f21d05949d48c4229b257ddc5de7
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2023-12-17 18:16:11 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -9,6 +9,10 @@
 # TTYD 自动登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
+# 32M 固件, https://www.bgegao.com/2020/11/1885.html
+# 第一条：这个很重要， 如果这条没有的话，那么软重启时（就是不拔电，用命令reboot重启，或者是系统里点重启按钮），你就要哭了，因为你的路由会变成黄灯一直卡在那里，别问我为什么，问我就想骂人了，我被这问题困扰了好久，找到好久才找到答案。这条命令的实际效果是在 mt7621_phicomm_k2p.dts 文件的 spi-max-frequency 这一行下，增加了一行 tbroken-flash-reset;
+# 第二条：是修改 mt7621_phicomm_k2p.dts ，将里面的 <0xa0000 0xf60000> 换成 <0xa0000 0x1fb0000>
+# 第三条：是修改 mt7621.mk ，将里面的磁盘大小 15744k 修改成 32448k，（也就是31MB的样子）
 sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
