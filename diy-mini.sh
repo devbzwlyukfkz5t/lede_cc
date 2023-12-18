7e1d5404cbb10cf52378ad60b6f91e79d0eb8572
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2023-12-18 14:03:13 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 32M 固件, https://www.bgegao.com/2020/11/1885.html
+sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
+
 # 修改默认IP
 # sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
@@ -9,13 +14,14 @@
 # TTYD 自动登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
-# 32M 固件, https://www.bgegao.com/2020/11/1885.html
-# 第一条：这个很重要， 如果这条没有的话，那么软重启时（就是不拔电，用命令reboot重启，或者是系统里点重启按钮），你就要哭了，因为你的路由会变成黄灯一直卡在那里，别问我为什么，问我就想骂人了，我被这问题困扰了好久，找到好久才找到答案。这条命令的实际效果是在 mt7621_phicomm_k2p.dts 文件的 spi-max-frequency 这一行下，增加了一行 tbroken-flash-reset;
-# 第二条：是修改 mt7621_phicomm_k2p.dts ，将里面的 <0xa0000 0xf60000> 换成 <0xa0000 0x1fb0000>
-# 第三条：是修改 mt7621.mk ，将里面的磁盘大小 15744k 修改成 32448k，（也就是31MB的样子）
-sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
+# 科学上网插件
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+svn export https://github.com/haiibo/packages/trunk/luci-app-vssr package/luci-app-vssr
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
+svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
