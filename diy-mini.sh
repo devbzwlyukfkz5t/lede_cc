5acad1cdbe0ab7e69fa51d9216e5dc88b3b7b23c
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2024-01-09 11:48:49 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -6,7 +6,7 @@ sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt76
 sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.101.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
@@ -16,7 +16,6 @@ sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
 
 # 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
-
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
