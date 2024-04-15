7e2a11c450eac1ec02abd82c41a158847ae5b4cd
jackyu2 <13330496+jackyu2@user.noreply.gitee.com>
2024-04-16 01:08:17 +0800

    *

--- diy-mini.sh
+++ diy-mini.sh
@@ -39,4 +39,4 @@ git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/l
 sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
+sed -i 's/15744k/32128k/g' target/linux/ramips/image/mt7621.mk
