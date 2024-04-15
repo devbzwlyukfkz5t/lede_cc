bbabf14bee304a388d24e5178b899b1914eecff2
jackyu2 <13330496+jackyu2@user.noreply.gitee.com>
2024-04-16 00:36:11 +0800

    *

--- diy-mini.sh
+++ diy-mini.sh
@@ -10,7 +10,7 @@ function git_sparse_clone() {
 }
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/192.168.102.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.102.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
@@ -40,4 +40,3 @@ sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
-
