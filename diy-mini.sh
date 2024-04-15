493cae6db7c018aafa208d78233e7d58c64cfd58
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2024-04-15 19:15:40 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -15,7 +15,7 @@ sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt76
 sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
 
 # 修改默认IP
-# sed -i 's/192.168.101.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.102.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
