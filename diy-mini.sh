ba963bd33922301ec72b683862420a90e237f53b
jackyu2 <13330496+jackyu2@user.noreply.gitee.com>
2024-04-16 11:40:07 +0800

    *

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
 # 32M 固件, https://www.bgegao.com/2020/11/1885.html
 sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
@@ -7,3 +17,6 @@ sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.102.1/g' package/base-files/files/bin/config_generate
+
+# 科学上网
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
