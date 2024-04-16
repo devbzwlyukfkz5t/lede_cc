c3554d6be326d9a3cdf3ef4bb32e44e8f05cbddd
jackyu2 <13330496+jackyu2@user.noreply.gitee.com>
2024-04-16 08:19:38 +0800

    *

--- diy-mini.sh
+++ diy-mini.sh
@@ -9,6 +9,12 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
+# 32M 固件, https://www.bgegao.com/2020/11/1885.html
+sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/15744k/32128k/g' target/linux/ramips/image/mt7621.mk
+
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.102.1/g' package/base-files/files/bin/config_generate
 
@@ -35,9 +41,3 @@ git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/l
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-# 32M 固件, https://www.bgegao.com/2020/11/1885.html
-sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/<80000000>/<10000000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/15744k/32128k/g' target/linux/ramips/image/mt7621.mk
