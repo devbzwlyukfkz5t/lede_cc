8db806f15c0f8821587bd2d16b361c59f119d669
jackyu2 <13330496+jackyu2@user.noreply.gitee.com>
2024-04-16 09:38:20 +0800

    *

--- diy-mini.sh
+++ diy-mini.sh
@@ -10,9 +10,9 @@ function git_sparse_clone() {
 }
 
 # 32M 固件, https://www.bgegao.com/2020/11/1885.html
+sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/32128k/g' target/linux/ramips/image/mt7621.mk
 
 # 修改默认IP
