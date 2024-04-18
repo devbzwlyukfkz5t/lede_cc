6ce3247b17530a2f004a8e065edd43539d4b0d36
jackyu2 <13330496+jackyu2@user.noreply.gitee.com>
2024-04-18 09:16:27 +0800

    *

--- r18.sh
+++ r18.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.101.2/g' package/base-files/files/bin/config_generate
 
 # 科学上网
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
