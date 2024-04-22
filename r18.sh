f66efe7eb8fbd95288a05784d2861233545e39ba
jackyu2 <13330496+jackyu2@user.noreply.gitee.com>
2024-04-22 17:16:01 +0800

    *

--- r18.sh
+++ r18.sh
@@ -5,3 +5,6 @@ sed -i 's/192.168.1.1/192.168.101.2/g' package/base-files/files/bin/config_gener
 
 # 科学上网
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+
+# mt7628 超频
+cp -f $GITHUB_WORKSPACE/scripts/999-mt7628-overclocking.patch target/linux/ramips/patches-5.15/999-mt7628-overclocking.patch
