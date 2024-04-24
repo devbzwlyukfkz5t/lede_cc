11481dc565cf7630f4eb4a6ce0e6b56a5274d593
jackyu2 <13330496+jackyu2@user.noreply.gitee.com>
2024-04-24 23:03:19 +0800

    *

--- ac1200gu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
-
-# mt7621 超频
-cp -f $GITHUB_WORKSPACE/scripts/999-mt7621-cpu-overclock.patch target/linux/ramips/patches-5.15/999-mt7621-cpu-overclock.patch
