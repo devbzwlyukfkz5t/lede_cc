6aefb5d0884f4d09c805d7ed833600b1478454c0
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2024-10-03 23:22:14 +0800

    Update r18.sh

--- r18.sh
+++ r18.sh
@@ -23,6 +23,7 @@ sed -i 's/192.168.1.1/192.168.101.2/g' package/base-files/files/bin/config_gener
 
 # 科学上网
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 
 # mt7628 超频
 cp -f $GITHUB_WORKSPACE/scripts/999-mt7628-cpu-overclock.patch target/linux/ramips/patches-5.15/999-mt7628-cpu-overclock.patch
