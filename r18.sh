6edab2e5e89a996c6ae8e5643538a60dc47a32a3
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2024-10-04 09:42:58 +0800

    Update r18.sh

--- r18.sh
+++ r18.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # 32M 固件, https://www.bgegao.com/2020/11/1885.html
-sed -i 's/<0x50000 0xf70000>/<0x50000 0xfb0000>/g' target/linux/ramips/dts/mt7628an_hiwifi_hc5x61a.dtsi
+sed -i 's/<0x50000 0xf70000>/<0x50000 0xfb0000>/g'    target/linux/ramips/dts/mt7628an_hiwifi_hc5x61a.dtsi
 sed -i 's/fc0000/1000000/g'              target/linux/ramips/dts/mt7628an_hiwifi_hc5x61a.dtsi
 sed -i 's/fe0000/1020000/g'              target/linux/ramips/dts/mt7628an_hiwifi_hc5x61a.dtsi
 sed -i 's/ff0000/1030000/g'              target/linux/ramips/dts/mt7628an_hiwifi_hc5x61a.dtsi
-sed -i 's/"HC5861B";/"HC5861B (32M)";/g' target/linux/ramips/dts/mt7628an_hiwifi_hc5861b.dts
+sed -i 's/"HiWiFi HC5861B";/"HiWiFi HC5861B (32M)";/g' target/linux/ramips/dts/mt7628an_hiwifi_hc5861b.dts
 sed -i 's/15808k/32448k/g'               target/linux/ramips/image/mt76x8.mk
+
+cat target/linux/ramips/dts/mt7628an_hiwifi_hc5861b.dts
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.101.2/g' package/base-files/files/bin/config_generate
@@ -18,5 +20,5 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 # mt7628 超频
 cp -f $GITHUB_WORKSPACE/scripts/999-mt7628-cpu-overclock.patch target/linux/ramips/patches-5.15/999-mt7628-cpu-overclock.patch
 
-./scripts/feeds update  -a
+./scripts/feeds update -a
 ./scripts/feeds install -a
