531c747bf5ac4641ce52f0616ece68e99c559601
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2024-10-07 23:51:50 +0800

    !

---
+++ rpi4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.101.2/g' package/base-files/files/bin/config_generate
+
+# 科学上网
+git clone --depth=1 https://github.com/fw876/helloworld.git                package/helloworld
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+
+./scripts/feeds update -a
+./scripts/feeds install -a
