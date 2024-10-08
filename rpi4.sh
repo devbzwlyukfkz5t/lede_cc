d52e087830cab9f098d4728885f42369f429b831
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2024-10-08 14:00:17 +0800

    !

--- rpi4.sh
+++ rpi4.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.101.2/g' package/base-files/files/bin/config_generate
 
 # 科学上网
 git clone --depth=1 https://github.com/fw876/helloworld.git                package/helloworld
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
