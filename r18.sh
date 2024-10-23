09b2ceff74b61fdee6331572403af8292f1d3733
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2024-10-23 14:47:56 +0800

    !

--- r18.sh
+++ r18.sh
@@ -8,7 +8,7 @@ sed -i 's/15808k/32448k/g'               target/linux/ramips/image/mt76x8.mk
 cat target/linux/ramips/dts/mt7628an_hiwifi_hc5861b.dts
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.101.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.4/g' package/base-files/files/bin/config_generate
 
 # 科学上网
 git clone --depth=1 https://github.com/fw876/helloworld.git                package/helloworld
